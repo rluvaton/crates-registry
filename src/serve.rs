74f3ea5e40fc56d79b31f0e41d52cd6200b75e65
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-08-06 10:30:59 +0300

    added support for publish as well

--- src/serve.rs
+++ src/serve.rs
@@ -203,7 +203,9 @@ pub async fn serve(root: &Path, binding: impl Into<ServerBinding>, server_addr: 
         .and(warp::path("v1"))
         .and(warp::path("crates"))
         .and(warp::path("new"))
-        .and(warp::put())
+        .and(
+            warp::put().or(warp::head()).unify()
+        )
         .and(warp::path::end())
         .and(warp::body::bytes())
         // We cap total body size to 20 MiB to have some upper bound. At the
